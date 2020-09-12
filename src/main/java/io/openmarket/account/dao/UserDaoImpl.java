38edc33d56d7aca34601ea28ce7d891c3af412f0
2OP <po>
2020-09-12 12:43:17 -0700

    unit tes

--- src/main/java/io/openmarket/account/dao/UserDaoImpl.java
+++ src/main/java/io/openmarket/account/dao/UserDaoImpl.java
@@ -1,10 +1,23 @@
 package io.openmarket.account.dao;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.document.KeyConditions;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.google.common.collect.ImmutableMap;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import io.openmarket.account.model.Account;
 import io.openmarket.dao.dynamodb.AbstractDynamoDBDao;
-import java.util.Optional;
+import javax.crypto.SecretKey;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 public class UserDaoImpl extends AbstractDynamoDBDao<Account> implements UserDao {
 
@@ -22,10 +35,40 @@ public class UserDaoImpl extends AbstractDynamoDBDao<Account> implements UserDao
         return Optional.empty();
     }
 
+    public static Optional<String> hash(byte[] password, byte[] salt) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedPassword = md.digest(password);
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashedPassword)
+                sb.append(String.format("%02x", b));
+            return Optional.of(sb.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
 
-    public Optional<String> login(String key) {
+    }
+
+    public int getCount (){
+        QueryRequest
+    }
+
+    public Optional<String> login(String username, byte[] password) {
         AmazonDynamoDB dbclient = getDbClient();
-        return null;
+        Map<String, String> name = ImmutableMap.of(":#u", "username");
+        Map<String, AttributeValue> value = ImmutableMap.of(":a", new AttributeValue(username));
+
+        QueryResult result = dbclient.query(new QueryRequest().withTableName("User").withExpressionAttributeNames(name)
+                .withExpressionAttributeValues(value).withKeyConditionExpression("#u = :a").withLimit(1));
+        if (result.getCount() == 0) return Optional.empty();
+        ByteBuffer passwordSalt = result.getItems().get(0).get("passwordSalt").getB();
+        ByteBuffer passwordSalted = result.getItems().get(0).get("password").getB();
+        if (!hash(password, passwordSalt.array()).get().getBytes().equals(passwordSalted)) return Optional.empty();
+        SecretKey key = Keys.hmacShaKeyFor(username.getBytes(StandardCharsets.UTF_8));
+        JwtBuilder jws = Jwts.builder().setSubject(username).signWith(key);
+        return Optional.of(jws.compact());
 
     }
 }
--- src/test/java/io/openmarket/example/HelloWorldTest.java
+++ src/test/java/io/openmarket/example/HelloWorldTest.java
@@ -1,5 +1,14 @@
 package io.openmarket.example;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperModelFactory;
+import io.openmarket.account.dao.UserDao;
+import io.openmarket.account.dao.UserDaoImpl;
+import io.openmarket.account.model.Account;
+import io.openmarket.dao.dynamodb.DynamoDBDao;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,5 +17,18 @@ public class HelloWorldTest {
     @Test
     public void testHello() {
         assertEquals("Hello!", new HelloWorld().sayHello());
+    }
+
+
+
+    @Test
+    public void testAccountDao () {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_WEST_2)
+                .build();
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+        DynamoDBDao<Account> dao = new UserDaoImpl(client, mapper);
+
+
     }
 }
